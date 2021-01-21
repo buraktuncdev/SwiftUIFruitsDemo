ae9d409827132004094e068f1b0ba51147be0b4b
Burak Tunc <burak.tunc@check24.de>
2021-01-21 13:42:21 +0100

    Initial Commit

---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Burak Tunc on 21.01.21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
